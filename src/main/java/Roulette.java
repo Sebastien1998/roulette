1db05333ed322a177e4f0a4f9e667111fb617f6a
Sebastien1998 <sebastien80001@gmail.com>
2021-05-07 21:23:34 +0200

    testing number generation

--- src/main/java/Roulette.java
+++ src/main/java/Roulette.java
@@ -1,2 +1,14 @@
+import static java.lang.Thread.sleep;
+
 public class Roulette {
+
+    int number;
+
+    public int tournerRoulette(){
+        return number = (int) (Math.random()*37);
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }
--- src/test/java/RouletteTest.java
+++ src/test/java/RouletteTest.java
@@ -1,5 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class RouletteTest {
 
+    @RepeatedTest(40)
+    public void tournerRouletteTest() {
+        Roulette roulette = new Roulette();
+        int number = roulette.tournerRoulette();
+        System.out.println(number);
+        Assertions.assertTrue(number<37&&number>-1);
+    }
 }
